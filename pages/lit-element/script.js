22c7f5a4b8d065c8aa5d9d1f1eee8ef9069df3b9
Siarhei Lunski <the.sergaunt@gmail.com>
2019-10-06 23:32:13 +0300

    Add async operation

--- pages/lit-element/script.js
+++ pages/lit-element/script.js
@@ -5,6 +5,7 @@ import {
   html,
   css
 } from 'https://unpkg.com/lit-element?module';
+import { until } from 'https://unpkg.com/lit-html/directives/until?module';
 
 class MyLoginForm extends LitElement {
   static get properties() {
@@ -12,7 +13,10 @@ class MyLoginForm extends LitElement {
       email: String,
       password: String,
       attempts: Number,
-      blocked: Boolean,
+      failed: Boolean,
+      successed: Boolean,
+      isLoading: Boolean,
+      messageRequest: String,
       // TODO: Если property могут быть любого типа, аттрибуты должны быть строками. Мы можем конвертировать их в соотвествующее property с нужным нам типом, передав в конструкторе флаг reflect: true
       attemptsAllowed: { type: Number, reflect: true }
     };
@@ -21,16 +25,21 @@ class MyLoginForm extends LitElement {
   // TODO: Сказать о том, что property можно инициализировать в конструкторе класса, или как аттрибут в самой разметке
   constructor() {
     super();
-    this.email = 'test@mail.com';
-    this.password = 'Testing123';
+    this.email = '';
+    this.password = '';
     this.hints = ['Enter your credentials', 'Press "Sign In"', 'Be happy'];
     this.attempts = 0;
-    this.blocked = false;
+    this.failed = false;
+    this.successed = false;
+    this.isLoading - false;
   }
 
   updated(changedProperties) {
-    if (changedProperties.has('blocked') && this.blocked) {
-      this.className = 'warning';
+    if (changedProperties.has('failed') && this.failed) {
+      this.className = 'failed';
+    }
+    if (changedProperties.has('successed') && this.successed) {
+      this.className = 'successed';
     }
   }
 
@@ -41,14 +50,20 @@ class MyLoginForm extends LitElement {
         display: inline-block;
         background-color: #f8f7f3;
         padding: 20px;
-        max-width: 300px;
+        width: 300px;
         border-radius: 10px;
         font-family: var(--myFontFamily, 'Arial Black');
       }
 
       /* TODO: Как в Angular */
-      :host(.warning) .counter {
+      :host(.failed) .counter,
+      :host(.failed) .message-request {
         color: red;
+      }
+
+      :host(.successed) .counter,
+      :host(.successed) .message-request {
+        color: #2e8b57;
       }
 
       ::slotted(img) {
@@ -64,11 +79,11 @@ class MyLoginForm extends LitElement {
         color: #1d1e22;
       }
 
-      .be-careful {
+      .counter span.be-careful {
         font-size: 16px;
         text-transform: uppercase;
-        margin: 0 auto;
         color: #fc9b3d;
+        margin-bottom: 0;
       }
 
       .counter {
@@ -112,6 +127,12 @@ class MyLoginForm extends LitElement {
         outline: 0;
       }
 
+      .message-request {
+        font-size: 16px;
+        margin-top: 10px;
+        color: #41a6f7;
+      }
+
       button {
         width: 100%;
         background-color: #41a6f7;
@@ -137,17 +158,32 @@ class MyLoginForm extends LitElement {
 
   handleSubmit(e) {
     e.preventDefault();
+    this.fetchMessage();
+  }
+
+  fetchMessage() {
     this.attempts += 1;
-    if (this.attempts >= this.attemptsAllowed) {
-      this.blocked = true;
-    }
-    /*     console.log('email', this.email);
-    console.log('password', this.password); */
+    this.messageRequest = new Promise(resolve => {
+      this.isLoading = true;
+      const { email, password } = this;
+      setTimeout(() => {
+        this.isLoading = false;
+        if (email === 'alexey@mail.com' && password === 'alexey') {
+          this.successed = true;
+          return resolve('Success');
+        }
+        if (this.attempts >= this.attemptsAllowed) {
+          this.failed = true;
+          return resolve('Failed');
+        }
+        resolve('Try again');
+      }, 1000);
+    });
   }
 
   /* TODO: Раccказать про computed property */
   get beCareful() {
-    return this.attempts === this.attemptsAllowed - 1
+    return this.attempts === this.attemptsAllowed - 1 && this.successed !== true
       ? html`
           <span class="be-careful">Be careful</span>
         `
@@ -158,44 +194,64 @@ class MyLoginForm extends LitElement {
     return html`
       <!--TODO: Указать разницу между property и attribute -->
       <!-- TODO: Слушатели событий устанавливаются либо через такие декларативы (@), через addEventListener в конструкторе и некоторых lifecycle методах  -->
-      <form @submit=${this.handleSubmit}>
+      <div class="counter">
         ${this.beCareful}
-        <div class="counter">
-          <span>Attempts</span>
-          <div>
-            ${this.attempts} / ${this.attemptsAllowed}
-          </div>
+        <span>Attempts</span>
+        <div>
+          ${this.attempts} / ${this.attemptsAllowed}
         </div>
-        <input
-          @input=${this.handleChange}
-          .value=${this.email}
-          ?disabled=${this.blocked}
-          name="email"
-          type="email"
-        />
-        <input
-          @input=${this.handleChange}
-          .value=${this.password}
-          ?disabled=${this.blocked}
-          name="password"
-          type="password"
-        />
-        <button ?disabled=${this.blocked} type="submit">Sign In</button>
-      </form>
-      <!-- TODO: Рассказать про conditionals и slots (light dom) -->
-      ${this.blocked
+      </div>
+      ${!this.successed
         ? html`
-            <slot name="warning"></slot>
+            <form @submit=${this.handleSubmit}>
+              <input
+                @input=${this.handleChange}
+                .value=${this.email}
+                ?disabled=${this.failed}
+                name="email"
+                type="email"
+              />
+              <input
+                @input=${this.handleChange}
+                .value=${this.password}
+                ?disabled=${this.failed}
+                name="password"
+                type="password"
+              />
+              <button ?disabled=${this.isLoading || this.failed} type="submit">
+                Sign In
+              </button>
+            </form>
           `
         : ''}
-      <ul>
+      <span class="message-request"
+        >${until(
+          this.messageRequest,
+          html`
+            Loading...
+          `
+        )}</span
+      >
+      <!-- TODO: Рассказать про conditionals и slots (light dom) -->
+      ${this.failed
+        ? html`
+            <slot name="failed"></slot>
+          `
+        : ''}
+      ${this.successed
+        ? html`
+            <slot name="success"></slot>
+          `
+        : ''}
+      <!-- TODO: Рассказать про циклы и маппинг -->
+      <!--       <ul>
         ${this.hints.map(
-          hint =>
-            html`
-              <li>${hint}</li>
-            `
-        )}
-      </ul>
+        hint =>
+          html`
+            <li>${hint}</li>
+          `
+      )}
+      </ul> -->
     `;
   }
 }
